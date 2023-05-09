49a98bc702c87601cb0cc2bcb6cd9aeebf153959
Brian Scherner <brianscherner@gmail.com>
2023-05-08 22:03:01 -0700

    Add addEventListener and define each variable for div class and form.

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function(){
+	let form = document.querySelector("form");
+	let shoegaze = document.getElementById("shoegaze");
+	let pastiche = document.getElementById("pastiche");
+	let folk = document.getElementById("folk");
+});
